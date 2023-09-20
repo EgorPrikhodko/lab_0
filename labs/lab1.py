4a913e8241f1173b7b3cebc62304e147ecd0384f
EgorPrikhodko <144201869+EgorPrikhodko@users.noreply.github.com>
2023-09-20 11:14:20 +0500

    Сделал 1 задание lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,3 +1,13 @@
 import math
+
 def task1():
-  pass
+  x = float(input())
+  y = float(input())
+  z = float(input())
+  
+  a = (sqrt(abs(x**0.5)))-sinh(y))/(1 + ln(x) - cosh(y))
+  b = 2*sin(x) + (sqrt(1 + ln(z))
+                  
+  print(a, b)
+  
+task1()
