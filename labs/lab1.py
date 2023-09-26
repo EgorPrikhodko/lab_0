582c59c7fdf1cd299910bac08e101da67dd00043
EgorPrikhodko <144201869+EgorPrikhodko@users.noreply.github.com>
2023-09-26 18:24:51 +0300

    скорректировал строки lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -3,12 +3,11 @@ def task1():
   x = float(input("x: "))
   y = float(input("y: "))
   z = float(input("z: "))
-  
   a = (math.sqrt(abs(x**0.5)))-(math.sinh(y))/(1 + math.log(x) - math.cosh(y))
   b = 2*math.sin(x) + (math.sqrt(1 + math.log(z)))
-
   print(a,b)
 task1()
+
 def task2():
   b = -1
   c = 2
