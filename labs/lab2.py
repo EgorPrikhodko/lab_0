3246f28c0114f56e5388c32a862debce06c33411
EgorPrikhodko <144201869+EgorPrikhodko@users.noreply.github.com>
2024-01-04 17:03:14 +0300

    Update lab2.py

--- labs/lab2.py
+++ labs/lab2.py
@@ -49,6 +49,47 @@ def task2():
 
 task2()
 
+def task3():
+    def toBASEint(num, base):
+        alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        n = abs(num)
+        b = alpha[n % base]
+        while n >= base:
+            n = n // base
+            b += alpha[n % base]
+        return ('' if num >= 0 else '-') + b[::-1]
+
+    def toBaseFrac(frac, base, n=16):
+        alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        b = ''
+        while n:
+            frac *= base
+            frac = round(frac, n)
+            b += str(alpha[int(frac)])
+            frac -= int(frac)
+            n -= 1
+        return b
+
+    Number = input("Число: ")
+    Basein = 10
+    Baseout = int(input("Введите СС: "))
+    alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if '.' in Number:
+        num, frac = map(str, Number.split('.'))
+        num = int(num, Basein)
+        a = toBASEint(num, Baseout)
+        b = 0
+        k = Basein
+        for i in frac:
+            b += alpha.index(i) / k
+            k *= Basein
+        b = str(toBaseFrac(b, Baseout)).rstrip('0')
+        print("Итог: ",a + '.' + b)
+    else:
+        print("Итог: ",toBASEint(int(Number, Basein), Baseout))
+
+task3()
+
 def task4():
     try:
         a = int(input("Введите первое число: "))
